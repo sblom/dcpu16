f29fca697f5fa3a3a163539b143bab6e012402d7
Scott Blomquist <github@scott.blomqui.st>
2012-04-04 10:35:37 -0700

    Stubs for DCPU16 VM in C#

---
+++ vm/csharp/dcpu16/vm/DCPU16.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dcpu16.VM
+{
+  public class Machine
+  {
+    enum Regs: byte { A, B, C, X, Y, Z, I, J };
+    ushort[] regs = new ushort[8];
+    ushort[] ram = new ushort[0x10000];
+    ushort pc, sp, o;
+  }
+
+  public class Processor
+  {
+    private Machine machine;
+
+    public Processor()
+      : this(new Machine())
+    {}
+
+    public Processor(Machine machine)
+    {
+      this.machine = machine;
+    }
+
+    public void DoCycle()
+    {
+
+    }
+  }  
+}
