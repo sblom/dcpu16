cdd81bc4a40f256b0eb92a8784daad3ad85202c3
Chad Blomquist <chad.blomquist@gmail.com>
2012-04-06 20:11:13 -0700

    forgot to track the new files

---
+++ vm/csharp/dcpu16/vm/Machine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dcpu16.VM
+{
+  public class Machine
+  {
+    public enum Regs : byte { A, B, C, X, Y, Z, I, J };
+    public ushort[] regs = new ushort[8];
+    public ushort[] ram = new ushort[0x10000];
+    public ushort pc, sp, o;
+    public bool skip;
+  }
+}
